157a18e60866c07fdd9d68a8ff4e6c1d79417e85
lyolya-a <lyolya-a@mail.ru>
2021-02-24 14:55:07 +0500

    let x

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("Hello, JavaScript!!!")
+//Это однострочный комментарий
+
+/* Это 
+многострочный 
+комментарий
+ */
+
+ let x = 1;
+ x = "Привет";
+ console.log(x)
+ x = true;
+ console.log(x)
+
+//console.log("Hello, JavaScript!!!")
