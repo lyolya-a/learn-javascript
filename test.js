ae4076b1fe69191c6296cfe0ad06d8e3de9ad2b9
lyolya-a <lyolya-a@mail.ru>
2021-02-24 16:56:30 +0500

    first lesson

--- test.js
+++ test.js
@@ -5,10 +5,68 @@
 комментарий
  */
 
- let x = 1;
+ let x;
+ let y;
+ let z;
  x = "Привет";
- console.log(x)
- x = true;
- console.log(x)
+ console.log("x"||x)
+ y = 1;
+ console.log(y)
 
+ z = x;
+ x = y;
+ console.log(x)
+ y = z;
+ console.log(y)
+
+
+let a = 5, b = 6;
+[a, b] = [b, a];
+console.log(`${a} ${b}`);
+
+let s = String(a);
+console.log(s); //6
+
+
+//Операторы
+/*let counter = 1;
+let nc = ++counter;
+console.log(nc);*/
+
+let counter = 1;
+let nc = counter++;
+console.log(nc);
+
+
+let n = 1;
+n += 2;
+console.log(n);
+n *= 3;
+console.log(n);
+
+console.log(+true);
+console.log(+"");
+
+console.log(9=="9");
+console.log(9==="9");
+
+console.log("Кят" > "Кот");
+
+console.log("25" > 6);
+
+let dec = 46 > 5 ? "МЭЭЭУ" : "Щинанайна";
+console.log(dec);
 //console.log("Hello, JavaScript!!!")
+
+//Цикл
+let n1 = 1;
+let n2 = 1;
+for (let i=3; i < 26; i++) {
+    let next = n1 + n2;
+    n1 = n2;
+    n2 = next;
+    console.log(n2);
+
+}   
+//console.log(n2);
+
