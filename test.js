ab73aaa4903ee2035e04e5abfdb1f9fb590d89ec
lyolya-a <lyolya-a@mail.ru>
2021-02-26 16:17:35 +0500

    26.02.2021

--- test.js
+++ test.js
@@ -1,183 +1,211 @@
-//вложенные циклы
-let result = "    ";
+//Объекты
+let cat = {
+    name: "Mark",
+    breed: "Dvoryanin",
+    weight: 5,
+    isAngry: false
+};
 
-for (let i = 10; i < 21; i++){
-    result += i + "  ";
+console.log(cat.isAngry);
+
+delete cat.isAngry;
+
+console.log(cat["weight"]);
+
+let propName = "name";
+console.log(cat[propName]);
+
+for (let prop in cat){
+    console.log(prop +" : "+ cat[prop]);
 }
-console.log(result);
+
+console.log(cat);
 
 
-for (let x = 10; x < 21; x++){
-    result = x + " ";
-    for (let y = 10; y < 21; y++){
-        result += x*y + " ";
+let cat2 = {
+    name : "Marfa"
+};
+
+console.log(cat2.name);
+
+let new_cat2 = cat2;
+new_cat2.name = "Izzy";
+
+console.log(cat2.name);
+
+//функция-конструктор объекта
+function Cat(name, color, fil_hunger) {
+    //свойства
+    this.name = name;
+    this.color = color;
+    this.fil_hunger - fil_hunger;
+
+    //метод
+    this.eat = function (food) {
+        let hungryMessage;
+        if (food >= fil_hunger){
+            hungryMessage = "сыт, доволен";
+        }
+        else {
+            hungryMessage = "голоден, бесится";
+        }
+        console.log (this.color + " " + this.name + ", съев " + food + " кг еды, " + hungryMessage);
     }
 
-    console.log(result);
-
 }
 
+let manul = new Cat("Мурзик", "рыжий", 10);
+let britain = new Cat("Роджер", "чёрный", 5);
 
-//проверка на простое число
-let n = 10;
-let s = 2;
-let flag = 0;
-while (s < n && flag == 0) {
- if (n%s==0){
-    flag = 1;
-  break;  
-} 
- s++;
+manul.eat(3);
+britain.eat(8);
+
+console.log(manul instanceof Cat);
+console.log(britain instanceof Cat);
+
+//прототип
+Cat.prototype.sayMeow = function() {
+    console.log("Мяу!");
+};
+manul.sayMeow();
+
+//Классы
+class Cat_new {
+    constructor(name, color, fil_hunger){
+        this.name = name;
+        this.color = color;
+        this.fil_hunger = fil_hunger;
+        }
+
+    eat(food) {
+          let hungryMessage;
+          if (food >= this.fil_hunger){
+              hungryMessage = "сыт, доволен";
+          }
+          else {
+              hungryMessage = "голоден, бесится";
+          }
+          console.log (this.color + " " + this.name + ", съев " + food + " кг еды, " + hungryMessage);
+      }
+};
+
+let meinkun = new Cat_new ("Пушок", "белый", 9);
+meinkun.eat(10);
+
+//Наследование
+class Animal {
+    constructor(name) {
+        this.name = name;
+        this.hasTail - true;
+    }
+
+    say() {
+        console.log("Привет!");
+    }
+};
+
+class Kitty extends Animal {
+    constructor(name, color){
+        super(name);
+        this.color = color;
+    }
+
+    say() {
+        console.log("Мяу!");
+    }
 }
-if (flag == 1) {
-    console.log("Число "+n+" не простое!"); 
-}
-else {
-    console.log("Число "+n+" является простым!"); 
+
+class Horse extends Animal {
+    constructor(name){
+        super(name);
+    }
+
+    say(){
+        console.log("Игого!");
+    }
+};
+
+class Dog extends Animal {
+    constructor(name, owner){
+        super(name);
+        this.owner = owner
+    }
+
+    say(){
+        console.log("Гав!")
+    }
 };
 
 
-//определить время года по месяцу
-let month = 2;
-let month_str;
-switch(month) {
-case 12:month_str = month+' - Декабрь - Зимний месяц';
-        break;
-case 1: month_str = month+' - Январь - Зимний месяц';
-        break;
-case 2: month_str = month+' - Февраль - Зимний месяц';
-        break;
+let kitty = new Kitty("Бьянка","трёхцветка");
+let horse = new Horse("Лолита");
+let dog = new Dog("Анфиса", "Алексей");
 
-case 3: month_str = month+' - Март - Весенний месяц';
-        break;
-case 4: month_str = month+' - Апрель - Весенний месяц';
-        break;
-case 5: month_str = month+' - Май - Весенний месяц';
-        break;
+kitty.say();
+horse.say();
+dog.say();
+console.log(dog.owner);
 
-case 6: month_str = month+' - Июнь - Летний месяц';
-        break;
-case 7: month_str = month+' - Июль - Летний месяц';
-        break;
-case 8: month_str = month+' - Август - Летний месяц';
-        break;
 
-case 9: month_str = month+' - Сентябрь - Осенний месяц';
-        break;
-case 10: month_str = month+' - Октбярь - Осенний месяц';
-         break;
-case 11: month_str = month+' - Ноябрь - Осенний месяц';
-         break;
+//поиск счастливых билетов
+function isLucky(n) {
+    let str = String(n).padStart(6, "0");
+    let sum1 = Number(str[0])+Number(str[1])+Number(str[2]);
+    let sum2 = Number(str[3])+Number(str[4])+Number(str[5]);
+    return sum1 === sum2;
 }
-console.log(month_str);
 
+let lucky_count = 0;
+for (let i = 0; i < 1000000; i++) {
 
-
-//функция на простоту числа
-function checkSimple (new_n) {
-    let s = 2;
-    let flag = 0;
-
-    while (s < new_n && flag == 0) {
-     if (new_n%s==0){
-        flag = 1;
-    } 
-     s++;
+    if (isLucky(i)){
+        lucky_count++;
     }
-    if (flag == 1) {
-        console.log("Число "+new_n+" не простое!"); 
-    }
-    else {
-        console.log("Число "+new_n+" является простым!"); 
+}
+console.log(lucky_count);
+
+
+//try catch throw
+try {
+    console.log(lalala);
+}
+catch(err){
+    console.log("ОШИБАААААААА");
+}
+
+throw new Error("Моя ошибка № 5");
+
+
+//promise
+let catMood = new Promise(
+    function (resolve, reject) {
+    let isHappy = Math.random()>=0.5;
+    if (isHappy) {
+    var meow = {
+    text: "Мяу ! ",
+    length: Math.trunc((Math.random()+0.1)*10)
     };
-};
-checkSimple(4);
-
-
-//функция на месяц
-function checkMonth (month) {
-    let month_str;
-switch(month) {
-case 12:month_str = month+' - Декабрь - Зимний месяц';
-        break;
-case 1: month_str = month+' - Январь - Зимний месяц';
-        break;
-case 2: month_str = month+' - Февраль - Зимний месяц';
-        break;
-
-case 3: month_str = month+' - Март - Весенний месяц';
-        break;
-case 4: month_str = month+' - Апрель - Весенний месяц';
-        break;
-case 5: month_str = month+' - Май - Весенний месяц';
-        break;
-
-case 6: month_str = month+' - Июнь - Летний месяц';
-        break;
-case 7: month_str = month+' - Июль - Летний месяц';
-        break;
-case 8: month_str = month+' - Август - Летний месяц';
-        break;
-
-case 9: month_str = month+' - Сентябрь - Осенний месяц';
-        break;
-case 10: month_str = month+' - Октбярь - Осенний месяц';
-         break;
-case 11: month_str = month+' - Ноябрь - Осенний месяц';
-         break;
-}
-return month_str;
-};
-console.log(checkMonth(5));
-
-
-//рекурсия
-function Fibbonaci(n){
+    resolve(meow); // Успех
+    } else {
+    var reason = new Error('Cat is angry');
+    reject(reason); // Отказ
+    }
     
-    if (n<3){
-        return 1;
     }
-    else {
-        return Fibbonaci(n-1)+Fibbonaci(n-2);
+    );
+    
+    catMood
+    .then(function (result) {
+    //resolve
+    console.log(result)
+    let s="";
+    for (let i = 0; i < result.length; i++) {
+    s=s+result.text;
     }
-};
-console.log(Fibbonaci(25));
-
-
-//анонимная функция
-let sayHello = function(msg){
-    return "Hello, "+ msg;
-};
-console.log(sayHello("World"));
-
-//анонимная функция-стрелка
-let sayHello2 = (msg) => {
-    return "Hello, "+ msg;
-};
-console.log(sayHello2("Alenka"));
-
-let sayHello3 = (msg) => console.log("Hello, "+msg);
-sayHello3("Jacky");
-
-
-//Callack
-function sleep()
-{
-    console.log("Mrrrrrrrrr");
-};
-
-function eat()
-{
-    console.log("Omnomnom");
-};
-
-let catLife = function(time, sleep, eat) {
-    if (time%2==0){
-        return eat();
-    }
-    else {
-        return sleep();
-    }
-};
-
-catLife(4, sleep, eat);
+    console.log(s);
+    
+    })
+    .catch(function (error) {
+    // reject
+    console.log(error.message);
+    });
